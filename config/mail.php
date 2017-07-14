12de5103458c577a9db7d782c7eb3c051c7d10ab
Kedar V <kedar54@gmail.com>
2017-07-13 20:35:01 -0400

    cleanup password reset mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name'    => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@launchpadcs.org'),
+        'name'    => env('MAIL_FROM_NAME', 'LaunchPad'),
     ],
 
     /*
